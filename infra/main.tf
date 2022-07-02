63ebdc358b85370c67d34d735df4a9fb34189931
Benjamin Bode <benni.bode@freenet.de>
2022-07-02 21:38:08 +0000

    WIP

--- infra/main.tf
+++ infra/main.tf
@@ -14,4 +14,9 @@ resource "azurerm_app_service" "app" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
