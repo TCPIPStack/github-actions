aca1475cf4d643b5ca9d59ff2dc423ef26a89552
Benjamin Bode <benni.bode@freenet.de>
2022-07-03 12:57:22 +0000

    WIP

--- infra/main.tf
+++ infra/main.tf
@@ -3,20 +3,23 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "appservice"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "github-actions-fme-bu4"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  service_plan_id      = azurerm_service_plan.plan.id
 
-  sku {
-    tier = "Standard"
-    size = "S1"
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
   }
 }
--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,4 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "hostname" {
+  value = azurerm_linux_web_app.app.default_hostname
 }
+
