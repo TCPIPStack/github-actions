19132e5d42a3e8c28b643bb0b6e2369278cbe961
Benjamin Bode <b.bode@fme.de>
2022-07-04 21:56:36 +0200

    WIP

--- infra/main.tf
+++ infra/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_linux_web_app" "app" {
   name                = "github-actions-fme-bu4"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id      = azurerm_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
     application_stack {
